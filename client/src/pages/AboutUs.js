9f10ef544eb0ca53e05e0c64fe7a51a365e8a9b8
stephp23 <stephp23@gmail.com>
2021-06-23 13:29:08 -0400

    font changes and picture formatting

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-// import StephaniePena from "../images/StephaniePena.jpeg"
+import StephaniePena from "../images/StephaniePena.jpeg"
 
 function AboutUs() {
 	const LinkedIn = <FontAwesomeIcon icon={faLinkedin} />;
@@ -19,11 +19,12 @@ function AboutUs() {
 			</div>
 			<div className="border flex p-10 flex-wrap">
 				<div className="flex flex-wrap xl:w-1/3 p-10 justify-center">
-					<h2 className="text-4xl font-bold font-bodySans py-10 ">
+					
+				</div>
+        <div className="flex flex-wrap xl:w-1/3 p-10 justify-center">
+          <h2 className="text-4xl font-bold font-bodySans py-10 ">
 						LET. ME. LIVE. is an independent service for reporting tenant harassment in New York City.
 					</h2>
-				</div>
-				<div className="flex flex-wrap xl:w-1/3 p-10 justify-center">
 					<h3 className="text-3xl font-bold font-bodySans py-5">Our Purpose</h3>
 					<ul>
 						<li className="text-2xl font-bodySans py-3">
@@ -34,8 +35,9 @@ function AboutUs() {
               the impact of COVID-19, there is another unfolding crisis of tenant harassment and displacement that is disproportionately
               affecting low-income and immigrant communities.
             </li>
-					</ul><div className="team">
-						<h3 classname="text-3xl font-bold font-bodySans py-5">LET. ME. LIVE. Team</h3>
+          </ul>
+          <div className="team">
+						<h3 classname="text-10xl font-bold font-bodySans py-5">LET. ME. LIVE. Team</h3>
 							<div className="team-role">
 								<img src="https://picsum.photos/id/237/200/300" alt="profile" />
 								<h2 className="pt-3 text-bg-charcoal text-xl font-bold">Shafee Ahmed</h2>
@@ -46,7 +48,7 @@ function AboutUs() {
 								</div>
 							</div>
 							<div className="team-role">
-								<img src="https://picsum.photos/id/237/200/300" alt="profile" />
+								<img src={StephaniePena} alt="profile" />
 								<h2 className="pt-3 text-bg-charcoal text-xl font-bold">Stephanie Peña</h2>
 								<h3 className="pt-3 text-bg-charcoal text-xl">Front End Developer</h3>
 								<div className="links">
@@ -77,7 +79,7 @@ function AboutUs() {
 						</div>
 				</div>
 			</div>
-		</div>
+		 </div>
 	);
 }
 
